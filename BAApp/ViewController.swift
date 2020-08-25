30d6277728583d0b0dd5cd04fdd3ce8a19e5c0f8
JOSE CRUZ GALINDO MARTINEZ <jose.galindo@ciencias.unam.mx>
2020-08-24 19:20:46 -0500

    Initial Commit

---
+++ BAApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BAApp
+//
+//  Created by Jose Galindo martinez on 24/08/20.
+//  Copyright © 2020 JCG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
